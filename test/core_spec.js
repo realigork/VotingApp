0ac1dc28fc32893b6dd09bff70af4307be868952
Igor Kolga <igorkolga@hotmail.com>
2018-07-05 20:33:10 +0100

    Add vote logic that creates a tally

--- test/core_spec.js
+++ test/core_spec.js
@@ -1,7 +1,7 @@
 import { List, Map } from 'immutable';
 import { expect } from 'chai';
 
-import { setEntries, next } from '../src/core';
+import { setEntries, next, vote } from '../src/core';
 
 const MOVIES = {
   'Trainspotting': 'Trainspotting',
@@ -37,6 +37,53 @@ describe('application logic', () => {
           pair: List.of(MOVIES['Trainspotting'], MOVIES['28 Days Later'])
         }),
         entries: List.of(MOVIES['Sunshine'])
+      }));
+    });
+  });
+
+  describe('vote', () => {
+    it('creates a tally for the voted entry', () => {
+      const state = Map({
+        vote: Map({
+          pair: List.of(MOVIES['Trainspotting'], MOVIES['28 Days Later'])
+        }),
+        entries: List()
+      });
+      const nextState = vote(state, MOVIES['Trainspotting']);
+
+      expect(nextState).to.equal(Map({
+        vote: Map({
+          pair: List.of(MOVIES['Trainspotting'], MOVIES['28 Days Later']),
+          tally: Map({
+            'Trainspotting': 1
+          })
+        }),
+        entries: List()
+      }))
+    });
+
+    it('adds to existing tally for the voted entry', () => {
+      const state = Map({
+        vote: Map({
+          pair: List.of(MOVIES['Trainspotting'], MOVIES['28 Days Later']),
+          tally: Map({
+            'Trainspotting': 3,
+            '28 Days Later': 2
+          })
+        }),
+        entries: List()
+      });
+      const nextState = vote(state, MOVIES['Trainspotting']);
+
+      expect(nextState).to.equal(Map({
+        vote: Map({
+          pair: List.of(MOVIES['Trainspotting'], MOVIES['28 Days Later']),
+          tally: Map({
+            'Trainspotting': 4,
+            '28 Days Later': 2
+          })
+        }),
+        entries: List()
       }));
     });
   });
