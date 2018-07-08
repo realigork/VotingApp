98ffedeef137a3a5f6b0060ea8c45d5399a71ab0
Igor Kolga <igorkolga@hotmail.com>
2018-07-08 16:53:48 +0100

    Update client testing config and add an initial button test

--- voting-client/test/test_helper.js
+++ voting-client/test/test_helper.js
@@ -2,11 +2,11 @@ import jsdom from 'jsdom';
 import chai from 'chai';
 import chaiImmutable from 'chai-immutable';
 
-const doc = jsdom.jsdom('<!doctype html><html><body></body></html>');
-const win = doc.defaultView;
+const { JSDOM } = jsdom;
+const { document } = (new JSDOM(`<!DOCTYPE html><html><body></body>`)).window;
 
-global.document = doc;
-global.window = win;
+global.document = document;
+global.window = document.defaultView;
 
 Object.keys(window).forEach((key) => {
   if (!(key in global)) {
