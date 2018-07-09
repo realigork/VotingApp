d52895c398dc79ba289c4e27cd2db3a0dc5d0776
Igor Kolga <igorkolga@hotmail.com>
2018-07-09 21:48:01 +0100

    Show tally on results component

--- voting-client/src/components/Results/index.jsx
+++ voting-client/src/components/Results/index.jsx
@@ -1,9 +1,30 @@
 import React from 'react';
 
 export default class Results extends React.PureComponent {
+  constructor() {
+    super();
+    this.getVotes = this.getVotes.bind(this);
+  }
+
+  getVotes(entry) {
+    if (this.props.tally && this.props.tally.has(entry)) {
+      return this.props.tally.get(entry);
+    }
+    return 0;
+  }
+
   render() {
     return (
-      <div>Hello from results</div>
+      <div className='results'>
+        {this.props.pair.map(entry =>
+          <div key='entry' className='entry'>
+            <h1>{entry}</h1>
+            <div className='voteCount'>
+              {this.getVotes(entry)}
+            </div>
+          </div>
+        )}
+      </div>
     );
   }
 }
